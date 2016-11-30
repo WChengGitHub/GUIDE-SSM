97bc3880dbad7c82885c9494686fb49aafe15af8
zhouzebiao <447523426@qq.com>
2016-11-30 20:52:18 +0800

    abc

---
+++ src/main/java/com/guide/ssm/module/user/service/SysUserService.java
@@ -0,0 +1,7 @@
+package com.guide.ssm.module.user.service;
+
+import com.guide.ssm.module.user.pojo.SysUser;
+
+public interface SysUserService {
+	SysUser getById(Long id);
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -25,5 +25,6 @@
 
 <a href="/sysUserController/showUserToJSONById/1" target="_blank">查询用户信息并直接输出JSON数据</a>
 
+
 </body>
 </html>
